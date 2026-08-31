3cd7b36f6cc5994a619c71b03f823409bee56ff0
SENTHAMARAI <arthiarthi61246@gmail.com>
2026-08-31 18:52:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function openPage(page) {
+    alert("Opening: " + page);
+}
+
+function exploreNearby() {
+    alert("Finding places near you...");
+}
+
+function search() {
+    alert("Search clicked");
+}
+
+function notify() {
+    alert("Notifications clicked");
+}
+
+function home() {
+    alert("Home clicked");
+}
+
+function bookings() {
+    alert("Bookings clicked");
+}
+
+function safety() {
+    alert("Safety clicked");
+}
+
+function profile() {
+    alert("Profile clicked");
+}
